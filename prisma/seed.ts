b89250252b5995438cb1858fa48b7f4c5469c2a4
Mirim Kim <mirimkim00@gmail.com>
2026-04-09 21:34:27 -0400

    Workkshop db migration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -110,7 +110,7 @@ async function resetLocalSeedData() {
   await prisma.favorite.deleteMany({});
   await prisma.cart.deleteMany({});
   await prisma.watchedVideo.deleteMany({});
-  await prisma.workshopRegistration.deleteMany({});
+  await prisma.userWorkshop.deleteMany({});
   await prisma.orderItem.deleteMany({});
   await prisma.order.deleteMany({});
   await prisma.review.deleteMany({});
@@ -619,8 +619,10 @@ async function main() {
         locationOrUrl: 'North York centre',
         status,
         category: ws.category as WorkshopCategory,
-        instructorId: instructorId2,
-        thumbnail: '/img/course_image1.png'
+        thumbnail: '/img/course_image1.png',
+        instructors: {
+          create: [{ instructorId: instructorId2 }]
+        }
       }
     });
 
@@ -685,8 +687,10 @@ async function main() {
         locationOrUrl: 'North York centre',
         status,
         category: ws.category as WorkshopCategory,
-        instructorId: instructorId2,
-        thumbnail: ws.thumbnail
+        thumbnail: ws.thumbnail,
+        instructors: {
+          create: [{ instructorId: instructorId2 }]
+        }
       }
     });
 
